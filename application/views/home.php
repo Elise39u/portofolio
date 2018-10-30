bd9df26bae214560fb6b3be3056148e628668f93
Justin van de laar <justin555@live.nl>
2018-10-30 11:14:16 +0100

    update

---
+++ application/views/home.php
@@ -0,0 +1,9 @@
+<header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Welcome To mine portfolio</div>
+          <div class="intro-heading text-uppercase">It's Nice To Meet You</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="">Let me guide you</a>
+        </div>
+      </div>
+    </header>
